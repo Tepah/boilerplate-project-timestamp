518698f0887c8a1ad1c23256f555d9b8ccc7ee1e
Pete Potipitak <67202848+Tepah@users.noreply.github.com>
2023-07-23 01:12:03 -0700

    Finished Challenge

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
@@ -19,11 +19,34 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get('/api', (req, res) => {
+  res.json({'unix': Math.floor(new Date().getTime()), 'utc': new Date().toUTCString()})
+})
+
+app.get('/api/:date?', (req, res, next) => {
+  if (!new Date(req.params.date).getTime()) {
+    let dateInt = parseInt(req.params.date)
+    req.unix = dateInt;
+    req.date = new Date(dateInt);
+  } else if (!req.params.date) {
+    req.date = new Date();
+  } else {
+    req.unix = new Date(req.params.date).getTime();
+    req.date = new Date(req.params.date);
+  }
+  next();
+}, (req, res) => {
+  if (req.date.toString() !== "Invalid Date") {
+    res.json({unix: req.unix, utc: req.date.toUTCString()})
+  } else {
+    res.json({error: "Invalid Date"})
+  }
+});
 
 
 // listen for requests :)
